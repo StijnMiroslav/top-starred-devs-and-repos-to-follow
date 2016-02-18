d8120fc94ac7d45e7ee413c3b68ce6833a2b7a91
StijnMiroslav <StijnMiroslav@gmail.com>
2016-02-18 07:48:14 -0500

    Create filters

---
+++ src/filters.py
@@ -0,0 +1,22 @@
+filter_not_maintained = [
+    'https://github.com/shadowsocks/shadowsocks',
+    'https://github.com/KeyboardFire/mkcast',
+]
+
+filter_fonts = [
+    'https://github.com/adobe-fonts/source-code-pro',
+    'https://github.com/larsenwork/monoid',
+    'https://github.com/chrissimpkins/codeface',
+]
+
+filter_awesome_lists = [
+    'https://github.com/vinta/awesome-python',
+    'https://github.com/josephmisiti/awesome-machine-learning',
+    'https://github.com/minimaxir/big-list-of-naughty-strings',
+    'https://github.com/drduh/OS-X-Security-and-Privacy-Guide',
+    'https://github.com/donnemartin/awesome-aws',
+]
+
+filter_misc = [
+    '',
+]
